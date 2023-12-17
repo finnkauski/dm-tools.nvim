bb61ceb429f3d2a6048242817c5216b29300c86f
Art <iwiivi@gmail.com>
2023-12-17 21:20:17 +0000

    Basic implementation of the config generator is completed

--- lua/dm-tools/scenes/generator.lua
+++ lua/dm-tools/scenes/generator.lua
@@ -1,20 +1,23 @@
 local utils = require("dm-tools.utils")
 local lights = require("dm-tools.lights")
 local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
 local action_utils = require("telescope.actions.utils")
 
 local M = {}
 
--- {
---   "name": "Altar",
---   "lights": {
---     "9" : { "on": true, "bri": 149, "color_space_coordinates": [0.6176, 0.3136] },
---     "8" : { "on": true, "bri": 195, "color_space_coordinates": [0.172, 0.747] }
---   },
---   "spotify": { "volume_percent":100, "context_uri": "spotify:playlist:37i9dQZF1DXa71eg5j9dKZ" }
--- }
+--- Turn a received state from a light to a sendable state
+local state_to_sendable = function(state)
+  state.bri = state.brightness
+  state.brightness = nil
+  state.hue = nil
+  state.sat = nil
+  state.effect = nil
+  state.color_temperature = nil
+  return state
+end
 
+--- Convert a light with ID and other info
+--- into an entry for Telescope
 local light_to_entry = function(light)
   local display = light.id .. " " .. light.name
   return {
@@ -24,6 +27,7 @@ local light_to_entry = function(light)
   }
 end
 
+--- Given a scene entry, generate a preview for it
 local scene_previewer = function(entry)
   local info = {
     "ID: " .. entry.value.id,
@@ -61,6 +65,7 @@ local function table_to_lines(tbl)
   return lines
 end
 
+--- Once a scene is ready to display, do so.
 local function finalise(scene)
   -- new buffer in a split
   vim.cmd("vsplit")
@@ -73,39 +78,61 @@ local function finalise(scene)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, table_to_lines(scene))
 end
 
+--- add spotify data to scene
+local function add_links(scene)
+  if string.lower(vim.fn.input("Include links? ")) == "y" then
+    scene.links = {}
+  end
+  finalise(scene)
+end
+
+--- add spotify data to scene
+local function add_spotify(scene)
+  if string.lower(vim.fn.input("Include Spotify? ")) == "y" then
+    scene.spotify = {
+      volume_percent = 100,
+      context_uri = "",
+      shuffle = false,
+    }
+  end
+  add_links(scene)
+end
+
+--- Add lights to scene using telescope
 local function add_lights(scene)
-  -- we need to add a table to insert into
-  if not scene.lights then
-    scene.lights = {}
-  end
+  if string.lower(vim.fn.input("Include Hue Lights? ")) == "y" then
+    -- we need to add a table to insert into
+    if not scene.lights then
+      scene.lights = {}
+    end
 
-  local data = lights.all(true)
+    local data = lights.all(true)
 
-  local function attach_mappings(prompt_bufnr, map)
-    map({ "i", "n" }, "<Tab>", function()
-      actions.select_default()
-    end)
-
-    map({ "i", "n" }, "<CR>", function()
-      action_utils.map_selections(prompt_bufnr, function(entry, _)
-        scene.lights[entry.value.id] = entry.value
+    local function attach_mappings(prompt_bufnr, map)
+      map({ "i", "n" }, "<Tab>", function()
+        actions.toggle_selection(prompt_bufnr)
       end)
-      actions.close(prompt_bufnr)
-      -- Remove the lights field if it's empty
-      if not next(scene.lights) then
-        scene.lights = nil
-      end
-      finalise(scene)
-    end)
 
-    actions.select_default:replace(function()
-      actions.toggle_selection(prompt_bufnr)
-    end)
+      map({ "i", "n" }, "<CR>", function()
+        action_utils.map_selections(prompt_bufnr, function(entry, _)
+          scene.lights[entry.value.id] = state_to_sendable(entry.value.state)
+        end)
 
-    return true
+        actions.close(prompt_bufnr)
+        -- Remove the lights field if it's empty
+        if not next(scene.lights) then
+          scene.lights = nil
+        end
+        add_spotify(scene)
+      end)
+
+      return true
+    end
+
+    utils.new_picker(data, light_to_entry, attach_mappings, scene_previewer)
+  else
+    add_spotify(scene)
   end
-
-  utils.new_picker(data, light_to_entry, attach_mappings, scene_previewer)
 end
 
 --- Generate a scene JSON representation
